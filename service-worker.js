ad1a909095758206149ad024a06ae39e288b52b1
hillreinermichael <hillreiner.michael@googlemail.com>
2026-08-14 18:18:25 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,5 @@
-const CACHE_NAME = "u11-coach-v29.7";
+const CACHE_NAME="u11-coach-v30";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./u11-coach-icon-192.png","./u11-coach-icon-512.png"];
-self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",event=>{
-  if(event.request.method!=="GET") return;
-  event.respondWith(caches.match(event.request).then(cached=>{
-    if(cached) return cached;
-    return fetch(event.request).then(response=>{
-      const copy=response.clone();
-      caches.open(CACHE_NAME).then(c=>c.put(event.request,copy));
-      return response;
-    }).catch(()=>caches.match("./index.html"));
-  }));
-});
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE_NAME).then(x=>x.put(e.request,copy));return r;}).catch(()=>caches.match("./index.html"))));});
