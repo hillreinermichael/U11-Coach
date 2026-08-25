28c1afcb112e617bc7964c595866de1444cc9f95
hillreinermichael <hillreiner.michael@googlemail.com>
2026-08-25 18:25:12 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="u11-coach-v30.54";
+const CACHE_NAME="u11-coach-v1.0";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./u11-coach-icon-192.png","./u11-coach-icon-512.png","./u11-parcours-aufbau.png","./u11-beschleunigen-abbremser.png","./u11-reaction-chase-game.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
